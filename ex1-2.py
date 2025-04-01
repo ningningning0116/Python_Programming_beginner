1abed62b8928b5de91643be457b2881ad64c88d2
ningningning0116 <kriskris11655@gmail.com>
2025-04-01 19:33:18 +0800

    Update ex1-2.py

--- ex1-2.py
+++ ex1-2.py
@@ -1,3 +1,14 @@
 name = "Laura"
 age = 14
 print(name, age)
+
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+a = b = c = 10
+print(a, b, c)
+
+x = 10      # int
+y = 3.14    # float
+z = "Hello" # str
+print(type(x), type(y), type(z))
